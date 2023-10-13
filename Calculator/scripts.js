6ad6263e451d85a26dbe3aa2a68ad4e0c1d6cdca
ABHAY <369abhayg@gmail.com>
2023-10-13 19:40:55 +0530

    added some functionalities

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -15,7 +15,35 @@ let calculation=localStorage.getItem('calculation')|| '';
         localStorage.setItem('calculation',calculation);
         return;
       }
-      calculation+=operation;
+      if(operation==='+' || operation==='-' || operation==='*' || operation==='/' ){
+        calculation+=' '+operation+' ';
+      }
+      else{
+        calculation+=operation;
+      }
       displayCalculation.innerHTML=calculation;
       localStorage.setItem('calculation',calculation);
+    }
+
+    function pressedKey(key){
+      if(key<=9 ){
+        updateCalculator(key);
+      }
+      else if(key==='+' || key==='-'|| key==='*' || key==='/' || key==='.'){
+        updateCalculator(key);
+      }
+      else if(key==='Enter' || key==='='){
+        updateCalculator('=')
+      }
+      else if(key==='Backspace'){
+        calculation=Math.floor(calculation/10);
+        if(calculation==0){
+          calculation='';
+        }
+        displayCalculation.innerHTML=calculation;
+        localStorage.setItem('calculation',calculation);
+      }
+      else if(key==='Delete'){
+        updateCalculator('clear');
+      }
     }
